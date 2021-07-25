455302f5103437ebee3f95b8658d683e8b5fef6c
liang <cschengliang@gmail.com>
2021-07-25 22:04:50 +0800

    bfs

--- Graph/graph.cpp
+++ Graph/graph.cpp
@@ -13,8 +13,16 @@
  * 某节点直接连接的边数：Degree
  * Degree中指向节点的边数：In-Degree
  * Degree中从该节点出发的边数：Out-Degree
+ * 在有向图或者无向图中，使得图的所有定点之间是连通的所需要的最小权值只之和的子集：Minimun Spanning Tree(MST)
+ * 两个顶点之间没有相同的两条边和一个顶点没有连接之间的的边：Simple Graph
+ * 有向非循环图：Directed acyclic graph
+ * 带权图：Weighted Graph
+ * 完全图，每个顶点之间有边：Complete Graph
+ * 连通图：Connected graph
  *
- *
- *
+ * 图的数据结构实现方式
+ * 邻接矩阵，邻接表
+ * 图的遍历方式
+ * BFS，DFS
  */
 #include "graph.h"
