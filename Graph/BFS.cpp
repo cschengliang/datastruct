b5a04ca05e9852672f8c1ea5e422dbaf67f39e1f
liang <cschengliang@gmail.com>
2021-07-25 11:43:27 +0800

    bfs

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -4,6 +4,25 @@
 #define MAX_VERTEX_NUM 100
 bool visited[MAX_VERTEX_NUM];
 void BFSTraverse(Graph G){
-
+    for(i=0;i<G.vexnum;++i)
+        visited[i]= false;
+    InitQueue(Q);
+    for（i=0;i<G.vexnum;++i)
+    if(!visited[i])
+        BFS(G,i);
+}
+void BFS(Graph G,int v){
+    visit(v);
+    visited[v]= true;
+    Enqueue(Q,v);
+    while (!isEmpty(Q)){
+        DeQueue(Q,v);
+        for(w=FirstNeighbor(G,v);w>=0;w=NextNeighbor(G,v,w))
+            if (!visited[w]){
+                visit(w);
+                visited[w]= true;
+                EnQueue(Q,w);
+            }
+    }
 }
 
