fcf6f176a466248847026469a63f55ab4c63397c
liang <cschengliang@gmail.com>
2021-07-28 18:28:55 +0800

    Default Changelist

---
+++ cmake-build-debug/CMakeFiles/datastruct.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/datastruct.dir/Graph/Floyd.cpp.obj"
+  "datastruct.exe"
+  "datastruct.exe.manifest"
+  "datastruct.pdb"
+  "libdatastruct.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/datastruct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
